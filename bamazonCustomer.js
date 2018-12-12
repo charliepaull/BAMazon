336583a9e7f0f628d0c20e9bae726904023ad27f
Charlie Paull <charliepaull211@gmail.com>
2018-12-11 16:20:50 -0800

    getting response on object

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 // require adds in inquirer & mysql npm packages
 var inquiry = require("inquirer");
 var mysql = require("mysql");
+var divider = "\n------------------------------\n";
 
 // used to connect mysql database to .js file
 var connection = mysql.createConnection({
@@ -17,19 +18,45 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
   });
 
-//   connection.connect(function(err) {
-//     if (err) return err;
-//     bamazonItem();
-//   });
+// have user choose item from list using inquire
+var query = "SELECT * FROM products";
+    connection.query(query, function(err, response){
+    if (err) return err;
+    // console.log(response);
+    for (var i in response){
+        // START HERE - help format the object to show user all products
+        var final = `
+        ${response[i].id} | $${response[i].item_id} | Product Name: ${[i].product_name} 
+        | Dept: ${response[i].department_name} | Price: ${response[i].price}
+        Stock: ${response[i].stock_quantity}
+      `}
+      console.log(final);
+    connection.end();
+});
   
-function readProducts() {
-    console.log("Selecting all products...\n");
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      // Log all results of the SELECT statement
-      console.log(res);
-      connection.end();
-    });
-  }
+// /*use connection.query for all function to connect 
+// sql datbase & bamazonCustomer.js file data*/
+// function requestItem(){
+//     inquiry
+//     .prompt({
+//         name: "item",
+//         type: "input",
+//         message: "Which item do you want?"
+//     })
+//     .then(function(answer){
+//         console.log(answer.item);
+//         connection.query("SELECT * FROM products")
+//     })
 
-  readProducts();
+// // 
+// function readProducts() {
+//     console.log("Selecting all products...\n");
+//     connection.query("SELECT * FROM products", function(err, res) {
+//       if (err) throw err;
+//       // Log all results of the SELECT statement
+//       console.log(res);
+//       connection.end();
+//     });
+//   }
+
+// //   readProducts();
